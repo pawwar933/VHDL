cd758232fbe440330b90aca8d94f82fd2bb7e2e8
Manrul <manrul93@gmail.com>
2015-02-19 16:16:54 +0100

    Create przerzutnik_t.vhd

---
+++ przerzutniki/przerzutnik_t.vhd
@@ -0,0 +1,19 @@
+entity przerzutnik_t is
+port(T,C,R :in bit;
+		Q  : out bit);
+end przerzutnik_t;
+
+architecture przerzutnik of przerzutnik_t is
+begin 
+process(c,r)
+	variable tq:bit;
+	begin
+	if R = '1' then tq:='0';
+	elsif C'event and C='1' and T='1' then 
+	tq:= not tq;
+	end if;
+	Q<=tq;
+	end process;
+end przerzutnik;
+
+	
