c4bb545a22001c599fe759b9df65a7f161d71af3
Manrul <manrul93@gmail.com>
2015-03-27 23:02:33 +0100

    Update dekoder.vhd

--- 1/dekoder.vhd
+++ 1/dekoder.vhd
@@ -5,26 +5,26 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 
 entity dekoder is
-    Port ( odczyt_klawisza : in  STD_LOGIC_vector(10 downto 0);
+    Port ( koniec_odczytu : in  STD_LOGIC_vector(10 downto 0);
            s : out  STD_LOGIC_VECTOR(7 downto 0));
 end dekoder;
 
 architecture Behavioral of dekoder is
 
 begin
-dekoder7:	process(odczyt_klawisza)
+dekoder7:	process(koniec_odczytu)
 			begin 
 				case odczyt_klawisza is 
-				when "00001011011"=> s<="01001000";-- jedynka 
-				when "00001111011"=> s<="10111010"; --dwójka
-				when "00010011011"=> s<="11011010";--itd
-				when "00010010111"=> s<="01011100";
-				when "00010111011"=> s<="11010110";
-				when "00011011011"=> s<="11110110";
-				when "00011110111"=> s<="01001110";
-				when "00011111011"=> s<="11111110";
-				when "00100011011"=> s<="11011110";
-				when "00100010111"=> s<="11101110";
+				when "11000101100"=> s<="01001000";-- jedynka 
+				when "11000111100"=> s<="10111010"; --dwójka
+				when "11001001100"=> s<="11011010";--itd
+				when "11001001010"=> s<="01011100";
+				when "11001011100"=> s<="11010110";
+				when "11001101100"=> s<="11110110";
+				when "11001111010"=> s<="01001110";
+				when "11001111100"=> s<="11111110";
+				when "11010001100"=> s<="11011110";
+				when "11010001010"=> s<="11101110";
 				when others=> s<="00010000";
 				end case;
 			end process dekoder7;
