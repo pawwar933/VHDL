a5211cdc2a3db913b7f31b201d6576f4c5df6b14
Manrul <manrul93@gmail.com>
2015-03-28 13:44:46 +0100

    Update przerzutnik_t.vhd

--- przerzutniki/przerzutnik_t.vhd
+++ przerzutniki/przerzutnik_t.vhd
@@ -1,12 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity przerzutnik_t is
-port(T,C,R :in bit;
-	Q  : out bit);
+port(T,C,R :in std_logic;
+	Q  : out std_logic);
 end przerzutnik_t;
 
 architecture przerzutnik of przerzutnik_t is
 	begin 
 		process(c,r)
-			variable tq:bit;
+			variable tq:std_logic;
 		begin
 			if R = '1' then tq:='0';		--C'event and C='1'  -wykrywa narastające zbocze sygnału C
 			elsif C'event and C='1' and T='1' then  --  można też elsif C'event and C='1' then
