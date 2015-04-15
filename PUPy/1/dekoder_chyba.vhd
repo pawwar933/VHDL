3f7683e55ac1b3fe3432f11797dc237973993cb9
Michał <manrul93@gmail.com>
2015-04-15 08:02:26 +0200

    Update dekoder_chyba.vhd

--- PUPy/1/dekoder_chyba.vhd
+++ PUPy/1/dekoder_chyba.vhd
@@ -20,37 +20,37 @@ dekoder7:	process(trans)
 			begin 
 			--wait until (trans'event);
 							
-				if (kod_klawisza(8 downto 1)= x"16" and (tmp /=	 "01001000")) then 
+				if (kod_klawisza = "00110100011" and (tmp /=	 "01001000")) then 
 						 s<="01001000";
 						 tmp<="01001000";
-				elsif (kod_klawisza(8 downto 1)= x"1E" and (tmp /=	 "10111010")) then 
+				elsif (kod_klawisza = x"1E" and (tmp /=	 "10111010")) then 
 						 s<="10111010";
 						 tmp<="10111010";
-				elsif (kod_klawisza(8 downto 1)= x"26" and (tmp /=	 "11011010")) then 
+				elsif (kod_klawisza = x"26" and (tmp /=	 "11011010")) then 
 						 s<="11011010";
 						 tmp<="11011010";
-				elsif (kod_klawisza(8 downto 1)= x"25" and (tmp /=	 "01011100")) then 
+				elsif (kod_klawisza = x"25" and (tmp /=	 "01011100")) then 
 						 s<="01011100";
 						 tmp<="01011100";
-				elsif (kod_klawisza(8 downto 1)= x"2E" and (tmp /=	 "11010110")) then 
+				elsif (kod_klawisza = x"2E" and (tmp /=	 "11010110")) then 
 						 s<="11010110";
 						 tmp<="11010110";
-				elsif (kod_klawisza(8 downto 1)= x"36" and (tmp /=	 "11110110")) then 
+				elsif (kod_klawisza = x"36" and (tmp /=	 "11110110")) then 
 						 s<="11110110";
 						 tmp<="11110110";
-				elsif (kod_klawisza(8 downto 1)= x"3D" and (tmp /=	 "01001110")) then 
+				elsif (kod_klawisza = x"3D" and (tmp /=	 "01001110")) then 
 						 s<="01001110";
 						 tmp<="01001110";
-				elsif (kod_klawisza(8 downto 1)= x"3E" and (tmp /=	 "11111110")) then 
+				elsif (kod_klawisza = x"3E" and (tmp /=	 "11111110")) then 
 						 s<="11111110";
 						 tmp<="11111110";
-				elsif (kod_klawisza(8 downto 1)= x"46" and (tmp /=	 "11011110")) then 
+				elsif (kod_klawisza = x"46" and (tmp /=	 "11011110")) then 
 						 s<="11011110";
 						 tmp<="11011110";
-				elsif (kod_klawisza(8 downto 1)= x"45" and (tmp /=	 "11101110")) then 
+				elsif (kod_klawisza = x"45" and (tmp /=	 "11101110")) then 
 						 s<="11101110";
 						 tmp<="11101110";
-				elsif (kod_klawisza(8 downto 1)= x"F0" and (tmp /=	 "01001000")) then 
+				elsif (kod_klawisza = x"F0" and (tmp /=	 "01001000")) then 
 						 NULL;
 				else s<="00010000";
 				tmp<="00010000";
